8964d81da44e7007a2a06eb31406ac0879f61dc4
Lucifer <vivekrai3944@gmail.com>
2024-01-05 08:09:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Get the slider container element
+const slider = document.querySelector('.slider');
+
+// Function to activate the slider on button click
+function activateSlider(e) {
+  // Get all elements with the class 'item' inside the slider
+  const items = document.querySelectorAll('.item');
+
+  // Move to the next item when the element with class 'next' is clicked
+  if (e.target.matches('.next')) {
+    // Append the first item to the end of the slider
+    slider.append(items[0]);
+  }
+
+  // Move to the previous item when the element with class 'prev' is clicked
+  if (e.target.matches('.prev')) {
+    // Prepend the last item to the beginning of the slider
+    slider.prepend(items[items.length - 1]);
+  }
+}
+
+// Add a click event listener to the entire document
+document.addEventListener('click', activateSlider, false);
